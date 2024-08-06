4383c5e3172f5476697f4584921c04abae9f9213
Viraj Doshi <viraj.nd@somaiya.edu>
2024-08-05 22:55:11 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCache = "site-static-v1";
-const dynamicCache = "site-dynamic-v1";
+const staticCache = "site-static-v10";
+const dynamicCache = "site-dynamic-v10";
 
 //here each entry is the key to the value which will be the response of that key
 const assets = [
